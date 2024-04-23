f40de3274f9cd50ce9aaebeb2be6d6b676400593
devhaute <devhaute@gmail.com>
2024-04-24 04:53:17 +0900

    UIImageView 써보기

--- SwiftUIkit/ViewController.swift
+++ SwiftUIkit/ViewController.swift
@@ -9,13 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var titlelabel: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        titlelabel.text = "헬로 월드?"
-        titlelabel.textColor = .purple
+        cofigureTitleLabel()
+        cofigureMyImageView()
     }
+    
+    private func cofigureTitleLabel() {
+        titlelabel.text = "hello world"
+        titlelabel.textColor = .purple
+        titlelabel.font = .italicSystemFont(ofSize: 25)
+        titlelabel.sizeToFit()
+    }
+    
+    private func cofigureMyImageView() {
+        myImageView.image = UIImage(named: "myImage")
+        myImageView.contentMode = .scaleAspectFit
+    }
+    
     @IBAction func didTabCloseButton(_ sender: UIButton) {
         titlelabel.text = "헬로 월드"
         print("didTapButton")
