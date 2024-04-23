7cbecca17cb75317e9f99dff101e344390d57783
devhaute <devhaute@gmail.com>
2024-04-24 02:21:11 +0900

    StoryBoard 학습

---
+++ SwiftUIkit/MySampleLabel.swift
@@ -0,0 +1,20 @@
+//
+//  MySampleLabel.swift
+//  SwiftUIkit
+//
+//  Created by kai on 4/24/24.
+//
+
+import UIKit
+
+class MySampleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- SwiftUIkit/ViewController.swift
+++ SwiftUIkit/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var titlelabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        titlelabel.text = "헬로 월드?"
+        titlelabel.textColor = .purple
     }
-
-
+    @IBAction func didTabCloseButton(_ sender: UIButton) {
+        titlelabel.text = "헬로 월드"
+        print("didTapButton")
+    }
 }
 
